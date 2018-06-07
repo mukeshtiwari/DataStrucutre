916e3c47986a5033ec400a6b2e01a285fed2e2e5
Mukesh Tiwari <mukeshtiwari.iiitm@gmail.com>
2018-06-07 21:22:15 +1000

    still working on balance

--- avl-tree.rkt
+++ avl-tree.rkt
@@ -1,21 +1,22 @@
 #lang racket
 
 (define-struct avl-tree
-  (left-tree value bal right-tree)
+  (left-tree value height right-tree)
   #:transparent)
 
 
 
-(define (depth-avl-tree a-tree)
+(define (height-avl-tree a-tree)
   (match a-tree
-    [(avl-tree _ _ bal _) bal]))
+    ['Empty 0]
+    [(avl-tree _ _ height _) height]))
 
 (define (balance-factor left-tree right-tree)
-  (- (depth-avl-tree right-tree) (depth-avl-tree left-tree)))
+  (- (height-avl-tree right-tree) (height-avl-tree left-tree)))
 
 (define (search-avl-tree a-tree x)
   (match a-tree
-    [(avl-tree 'Empty y _ 'Empty) (eq? x y)] ;; I am at root node
+    ['Empty #f]
     [(avl-tree left-tree y _ right-tree)
      (cond
        [(eq? x y) #t] ;; value is present 
@@ -28,13 +29,32 @@
 
 (define (rotate-avl-tree a-tree)
   a-tree)
+  
 
 (define (avl-tree-insertion a-tree x)
   (match a-tree
     ['Empty (avl-tree 'Empty x 0 'Empty)]
-    [(avl-tree left-tree y b right-tree)
+    [(avl-tree left-tree y height right-tree)
      (cond
        [(eq? x y) a-tree]
-       [(< x y) (rotate-avl-tree (avl-tree (avl-tree-insertion left-tree x) y b right-tree))]
-       [else (rotate-avl-tree (avl-tree left-tree y b (avl-tree-insertion right-tree x)))])]))
-      
+       [(< x y)
+        (match-let ([(avl-tree ll-tree lx lh lr-tree)
+                     (avl-tree-insertion left-tree x)])
+          (rotate-avl-tree
+           (avl-tree (avl-tree ll-tree lx lh lr-tree)
+                     y
+                     (+ 1 (max lh (height-avl-tree right-tree)))
+                     right-tree)))]
+       [(> x y)
+        (match-let ([(avl-tree rl-tree rx rh rr-tree)
+                     (avl-tree-insertion right-tree x)])
+          (rotate-avl-tree
+           (avl-tree left-tree
+                     y
+                     (+ 1 (max (height-avl-tree left-tree) rh))
+                     (avl-tree rl-tree rx rh rr-tree))))])]))
+
+
+(define (tree-from-list lst)
+  (foldl (λ(x acc) (avl-tree-insertion acc x)) 'Empty lst))
+
