d46ab21e21316450527e3796cdf7c5ad16c3c6c9
Mukesh Tiwari <mukeshtiwari.iiitm@gmail.com>
2018-06-08 18:49:20 +1000

    Removed height from code

--- avl-tree.rkt
+++ avl-tree.rkt
@@ -1,23 +1,22 @@
 #lang racket
 
 (define-struct avl-tree
-  (left-tree value height right-tree)
+  (value left-tree right-tree)
   #:transparent)
 
 
-
-(define (height-avl-tree a-tree)
+(define (height-of-avl-tree a-tree)
   (match a-tree
     ['Empty 0]
-    [(avl-tree _ _ height _) height]))
+    [(avl-tree _ left-tree right-tree)
+     (+ 1 (max (height-of-avl-tree left-tree)
+               (height-of-avl-tree right-tree)))]))
 
-(define (balance-factor left-tree right-tree)
-  (- (height-avl-tree right-tree) (height-avl-tree left-tree)))
 
 (define (search-avl-tree a-tree x)
   (match a-tree
     ['Empty #f]
-    [(avl-tree left-tree y _ right-tree)
+    [(avl-tree y left-tree right-tree)
      (cond
        [(eq? x y) #t] ;; value is present 
        [(< x y) (search-avl-tree left-tree x)] ;; x < y go left
@@ -27,34 +26,30 @@
 ;; Idea here is you perform insertion in same way as bst
 ;; and then perform rotation on tree
 
-(define (rotate-avl-tree a-tree)
-  a-tree)
-  
-
-(define (avl-tree-insertion a-tree x)
+; https://upload.wikimedia.org/wikipedia/commons/c/c4/Tree_Rebalancing.gif
+(define (left-left-rotation a-tree)
   (match a-tree
-    ['Empty (avl-tree 'Empty x 0 'Empty)]
-    [(avl-tree left-tree y height right-tree)
-     (cond
-       [(eq? x y) a-tree]
-       [(< x y)
-        (match-let ([(avl-tree ll-tree lx lh lr-tree)
-                     (avl-tree-insertion left-tree x)])
-          (rotate-avl-tree
-           (avl-tree (avl-tree ll-tree lx lh lr-tree)
-                     y
-                     (+ 1 (max lh (height-avl-tree right-tree)))
-                     right-tree)))]
-       [(> x y)
-        (match-let ([(avl-tree rl-tree rx rh rr-tree)
-                     (avl-tree-insertion right-tree x)])
-          (rotate-avl-tree
-           (avl-tree left-tree
-                     y
-                     (+ 1 (max (height-avl-tree left-tree) rh))
-                     (avl-tree rl-tree rx rh rr-tree))))])]))
+   [(avl-tree x (avl-tree y left-left-tree left-right-tree) right-tree)
+    (avl-tree y left-left-tree (avl-tree x left-right-tree right-tree))]))
 
+(define (right-right-rotation a-tree)
+  (match a-tree
+    [(avl-tree x left-tree (avl-tree y right-left-tree right-right-tree))
+     (avl-tree y (avl-tree left-tree right-left-tree) right-right-tree)]))
+                              
+(define (left-right-rotation a-tree)
+  (match a-tree
+    [(avl-tree x (avl-tree y ll-tree
+                          (avl-tree z lll-tree llr-tree)) right-tree)
+     (avl-tree z
+               (avl-tree y ll-tree lll-tree)
+               (avl-tree x llr-tree right-tree))]))
+                             
+(define (right-left-rotation a-tree)
+  (match a-tree
+    [(avl-tree x left-tree (avl-tree y (avl-tree z rrl-tree rrr-tree) rr-tree))
+     (avl-tree z
+               (avl-tree x left-tree rrl-tree)
+               (avl-tree y rrr-tree rr-tree))]))
 
-(define (tree-from-list lst)
-  (foldl (λ(x acc) (avl-tree-insertion acc x)) 'Empty lst))
 
